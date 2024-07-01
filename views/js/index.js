07d9bc8ccb9e929183cfbff580472c54cfff94ef
Tayoid <idowutemitayoisaiah@gmail.com>
2024-07-01 12:10:35 +0100

    this should be my last commit if there is nothing wrong

--- views/js/index.js
+++ views/js/index.js
@@ -2,12 +2,12 @@ function sideBarOn() {
     const times = document.getElementById("close-button");
     const burger = document.getElementById("mobile-view");
     burger.addEventListener("click",()=>{
-        times.classList.replace("hidden","flex")})
+        times.classList.replace("hidden","absolute")})
 }
 
 function sideBarOff() {
     const times = document.getElementById("close-button");
     times.addEventListener("click",()=>{
-        times.classList.replace("flex","hidden")
+        times.classList.replace("absolute","hidden")
     })
 }
